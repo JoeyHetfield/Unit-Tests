62ad52d1f292e6506ee44b0aa8258377bce71bd1
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-21 12:25:15 -0300

    Feito requisito myCOunter, productDetails incompleto

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,24 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+        // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function')
+  });
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
+    it('Teste se o retorno da função é um array.', () => { 
+      expect(productDetails()).toEqual(array)
+    })
+    
+  
+   it('Teste se o array retornado pela função contém dois itens dentro.', () => { 
+      expect(productDetails().length).toBe(2)
+    })
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => { 
+      expect(productDetails('firstProduct', 'secondProduct')).toBe(object)
+    })
+    
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
-  });
+  
 });
